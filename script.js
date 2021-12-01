05f93d945181be3d9352b037a5b122a9b74201b3
BruHeroNxT <br.monteiro04@hotmail.com>
2021-12-01 19:29:57 -0300

    adicionando funções de scroll e adaptando tamanho da página

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var btn = document.querySelector("#back-to-top");
 btn.addEventListener("click", function(){
     window.scrollTo(0,0);
 });
+
+var btnContact = document.querySelector("#scroll-contact");
+
+btnContact.addEventListener("click", function(){
+    window.scrollTo(0,1730);
+});
+
+var btnAbout = document.querySelector("#scroll-about");
+
+btnAbout.addEventListener("click", function(){
+    window.scrollTo(0,800);
+});
